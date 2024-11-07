2ae5f6f8968cecdd4f001de3d5e3d0d2990f7387
Connor Trask <traskcon@msu.edu>
2024-11-06 19:04:40 -0500

    Finalized reddit scraping code

--- Data.py
+++ Data.py
@@ -1,9 +1,42 @@
 import praw
 import datetime as dt
+import pandas as pd
+import csv, io
+import re
+from tqdm import tqdm
 
 def get_date(submission):
     time = submission.created
     return dt.datetime.fromtimestamp(time)
+
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+        u"\uFFFD" #replacement character
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
+def make_safe_string(text):
+    text = remove_emojis(text)
+    # Remove newlines, tabs, and commas
+    text = text.replace("\n", "").replace("\t","").replace(",","")
+    return text
 
 reddit = praw.Reddit(
     client_id = "",
@@ -26,11 +59,21 @@ league_subreddits = {"NFL":["nfl"],
                      "NWSL":["nwsl"],
                      "WNBA":["wnba"]}
 
-for team in league_teams["NFL"]:
-    # Obtain subreddit instance
-    subreddit = reddit.subreddit(team)
-    # Obtain 100 newest posts
-    for post in subreddit.new(limit=100):
-        all_comments = post.comments.list()
-        print(get_date(all_comments[0]))
-    break
+
+with open("reddit_data.csv", "w", newline="", encoding="utf-8") as csv_file:
+    writer = csv.writer(csv_file, delimiter=",")
+    for team in league_teams["NFL"]:
+        # Obtain subreddit instance
+        subreddit = reddit.subreddit(team)
+        # Obtain 100 newest posts
+        for post in tqdm(subreddit.new(limit=100)):
+            all_comments = post.comments.list()
+            # comment.body returns the raw text of the comment
+            # Use get_date function to extract timestep of comment
+            # comment.id is a unique identifier for the comment
+            for comment in all_comments:
+                try:
+                    data = [team, comment.id, get_date(comment), make_safe_string(comment.body)]
+                    writer.writerow(data)
+                except:
+                    continue
