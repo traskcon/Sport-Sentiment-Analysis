485090371cc14f7877018f044ea85d84957d6b70
Connor Trask <traskcon@msu.edu>
2024-11-18 19:14:15 -0500

    Finished initial data pre-processing pipeline

--- Model.py
+++ Model.py
@@ -1,26 +1,36 @@
 import nltk
-import string
+nltk.download("stopwords")
+nltk.download("wordnet")
+from nltk.corpus import stopwords
+import pandas as pd
 import re
-import keras
+#import keras
 
-# Key NLP Pipeline Steps (not yet in order):
-# 1. Tokenize the text
-tokens = nltk.word_tokenize("text")
+def remove_tags(string):
+    result = re.sub('','',str(string))          #remove HTML tags
+    result = re.sub('https://.*','',result)   #remove URLs
+    result = re.sub(r"@\w+", "", result) #Remove handles
+    result = re.sub(r'\W+', ' ',result)    #remove non-alphanumeric characters 
+    result = result.lower() #Make text lowercase
+    return result
 
-# 2. Make text lowercase (case-insensitive)
-lowercased_tokens = [token.lower() for token in tokens]
+def lemmatize_text(text, w_tokenizer=nltk.tokenize.WhitespaceTokenizer(), lemmatizer=nltk.stem.WordNetLemmatizer()):
+    st = ""
+    for w in w_tokenizer.tokenize(text):
+        st = st + lemmatizer.lemmatize(w) + " "
+    return st
 
-# 3. Remove punctuation
-filtered_tokens = [token for token in tokens if token not in string.punctuation]
+def preprocess_data(data):
+    #NLP Data Pre-processing Pipeline:
+    # Remove HTML tags, URLs, etc.
+    data['comment'] = data['comment'].apply(lambda cw : remove_tags(cw))
+    # Remove stopwords
+    stop_words = set(stopwords.words("english"))
+    data['comment'] = data['comment'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_words)]))
+    # Lemmatize data
+    data['comment'] = data.comment.apply(lemmatize_text)
+    return data
 
-# 4. Remove stopwords
-stopwords = nltk.corpus.stopwords.words("english")
-filtered_tokes = [token for token in tokens if token.lower() not in stopwords]
-
-# 5. Remove URLs
-pattern = r"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
-cleaned_text = re.sub(pattern, "", "text")
-
-# 6. Remove handles
-pattern = r"@[^]+"
-cleaned_text = re.sub(pattern, "", "text")
+nhl_data = pd.read_csv("reddit_data_nhl.csv", names=["team","id","date","comment"])
+cleaned_data = preprocess_data(nhl_data)
+print(cleaned_data.head(10))
