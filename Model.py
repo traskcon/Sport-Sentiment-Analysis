64923b9aedab5d897d38d4d47ff53fa3a616c376
Connor Trask <traskcon@msu.edu>
2024-11-18 19:38:07 -0500

    Fixed URL regex

--- Model.py
+++ Model.py
@@ -8,7 +8,7 @@ import re
 
 def remove_tags(string):
     result = re.sub('','',str(string))          #remove HTML tags
-    result = re.sub('https://.*','',result)   #remove URLs
+    result = re.sub('http\S+','',result)   #remove URLs
     result = re.sub(r"@\w+", "", result) #Remove handles
     result = re.sub(r'\W+', ' ',result)    #remove non-alphanumeric characters 
     result = result.lower() #Make text lowercase
@@ -31,6 +31,6 @@ def preprocess_data(data):
     data['comment'] = data.comment.apply(lemmatize_text)
     return data
 
-nhl_data = pd.read_csv("reddit_data_nhl.csv", names=["team","id","date","comment"])
-cleaned_data = preprocess_data(nhl_data)
+sentiment_data = pd.read_csv("training.1600000.processed.noemoticon.csv", names=["sentiment","id","date","query","user","comment"])
+cleaned_data = preprocess_data(sentiment_data)
 print(cleaned_data.head(10))
