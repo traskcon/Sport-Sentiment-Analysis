0c45c8fafd067a6f878277ecf2e8b005501f0a36
Connor Trask <traskcon@msu.edu>
2024-11-26 17:26:23 -0500

    Fit initial LSTM model

--- Model.py
+++ Model.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import nltk
 nltk.download("stopwords")
 nltk.download("wordnet")
@@ -68,20 +70,19 @@ def build_model(train_comments):
                                             standardize=None,
                                             output_sequence_length=max_length)
     encoder.adapt(train_comments)
-    print(np.array(encoder.get_vocabulary())[:20])
     # model initialization
     #TODO: Add input layer to the sequential model
     model = keras.Sequential([
         encoder,
-        keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim),
+        keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim, mask_zero=True),
         keras.layers.Bidirectional(keras.layers.LSTM(64)),
         keras.layers.Dense(24, activation='relu'),
-        keras.layers.Dense(1, activation='sigmoid')
+        keras.layers.Dense(1, activation=None)
     ])
     # compile model
-    model.compile(loss='binary_crossentropy',
+    model.compile(loss='mse',
                 optimizer='adam',
-                metrics=['accuracy'])
+                metrics=['mse'])
     return model
 
 sentiment_data = pd.read_csv("training.1600000.processed.noemoticon.csv", names=["sentiment","id","date","query","user","comment"])
@@ -91,4 +92,6 @@ comments = cleaned_data["comment"].values
 labels = cleaned_data["sentiment"].values
 train_comments, test_comments, train_labels, test_labels = train_test_split(comments, labels, stratify = labels)
 model = build_model(train_comments)
+model.fit(train_comments, train_labels, epochs=3)
+model.save("initial_model.keras")
 print(model.summary())
