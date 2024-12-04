22e16de5da747af2570087aa8448b021e454009c
Connor Trask <traskcon@msu.edu>
2024-12-03 21:15:09 -0500

    Visualize initial results

--- Analysis.py
+++ Analysis.py
@@ -4,9 +4,12 @@ import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from Model import preprocess_data
 import pandas as pd
+import matplotlib.pyplot as plt
 import keras
 
 model = keras.models.load_model("initial_model.keras")
 data = pd.read_csv("reddit_data_nhl.csv", names=["team","id","timestamp","comment"])
 cleaned_data = preprocess_data(data)
 data["predicted sentiment"] = model.predict(cleaned_data["comment"])
+plt.hist(data["predicted sentiment"])
+plt.show()
